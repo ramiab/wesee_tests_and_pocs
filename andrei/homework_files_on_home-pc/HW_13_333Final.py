7247ae80e00d06513b8d609de90ed1916e9d9cc1
Andrei Budu <andrei.budu@wesee.com>
2015-11-15 20:51:17 +0200

    made some more changes

--- andrei/homework_files_on_home-pc/HW_13_333Final.py
+++ andrei/homework_files_on_home-pc/HW_13_333Final.py
@@ -1,48 +1,40 @@
 import os
-import sys
 import shutil
-#----------THIS WORKS-------------
-print ("This program lets you copy or move files or folders from one folder to another \n")
-
-SourcePath = r"C:\Users\Andrei64\Desktop\Python Homework\Python 13\tempFolder\FolderAwithFiles" #--input('Please enter your correct INPUT path: ')
-DestinationPath = r"C:\Users\Andrei64\Desktop\Python Homework\Python 13\tempFolder\FolderBWithFIles" #--input('Please enter your correct OUTPUT path: ')
-
-
-print ("\n This is your source path: \n",SourcePath)
-print ("This is your destination path: \n",DestinationPath)
-
-		
-choose_either_move_or_copy = int(input('Please enter \n 1 to COPY FILES \n 2 to MOVE FILES \n 3 to COPY FOLDERS \n 4 to MOVE FOLDERS \n'))
-print ("You chose option nr \n", choose_either_move_or_copy + 0)
-
-onlyfiles = [ os.path.join(SourcePath,f) for f in os.listdir(SourcePath) if os.path.isfile(os.path.join(SourcePath,f)) ]
-onlyfolders = [ os.path.join(SourcePath,f) for f in os.listdir(SourcePath) if os.path.isdir(os.path.join(SourcePath,f)) ]
-
-number_of_items = 0
 
 def copy_and_paste():
-	if choose_either_move_or_copy == 1 or 2 or 3 or 4:
-		global number_of_items
-		if choose_either_move_or_copy == 3:                      #--copy returns FileExistsError
-			for full_file_name in onlyfolders:
-				number_of_items = number_of_items + 1
-				shutil.copy(full_file_name, DestinationPath)
-		elif choose_either_move_or_copy == 4:
-			for full_file_name in onlyfolders:
-				number_of_items = number_of_items + 1
-				shutil.move(full_file_name, DestinationPath)
-		elif choose_either_move_or_copy == 1:
-			for full_file_name in onlyfiles:
-				number_of_items = number_of_items + 1
-				shutil.copy(full_file_name, DestinationPath)
-		elif choose_either_move_or_copy == 2:
-			for full_file_name in onlyfiles:
-				number_of_items = number_of_items + 1
-				shutil.move(full_file_name, DestinationPath)
+
+	SOURCEPATH = r"C:\Users\WeSEE 15\Desktop\Python HW 12\Python 12 HW\Python 12 HW\tempFolder\FolderAwithFiles"
+	DESTINATIONPATH =r"C:\Users\WeSEE 15\Desktop\Python HW 12\Python 12 HW\Python 12 HW\tempFolder\FolderOutput"
+
+	print ("This program lets you copy or move files from one folder to another \n")
+	print ("This is your source path: \n",SOURCEPATH)
+	print ("This is your destination path: \n",DESTINATIONPATH)
+	print ('\n')
+	
+	
+	print ("These are the files affected by the script :\n")
+	onlyfiles = [ os.path.join(SOURCEPATH,f) for f in os.listdir(SOURCEPATH) if os.path.isfile(os.path.join(SOURCEPATH,f)) ]
+	print (onlyfiles)
+	print ('\n')
+	print ("These folders are not affected by the script :")
+	onlyfolders = [ f for f in os.listdir(SOURCEPATH) if os.path.isdir(os.path.join(SOURCEPATH,f)) ]
+	print (onlyfolders)
+	print ('\n')
+	
+	
+	choose_either_move_or_copy = int(input('Please enter 1 to copy your files or enter 2 to move them: \n'))
+	print ("You chose option nr \n", choose_either_move_or_copy + 0)
+
+	#src_files = os.listdir(SOURCEPATH) #--Works till here--
+	#print ("These are the files and folders present in the directory \n", src_files)
+
+	for full_file_name in onlyfiles:
+		if choose_either_move_or_copy == 1:                    #----The integer input works
+			shutil.copy(full_file_name, DESTINATIONPATH)
+		elif choose_either_move_or_copy == 2: 
+			shutil.move(full_file_name, DESTINATIONPATH)				
 		else:
-			print ("You did not insert a valid number.Please run the script again.")			
+			print ("You did not insert a valid number.Please run script again.")      #-----WORKS!!---
 copy_and_paste()
 
-print ('You worked with:'+ str(number_of_items)+' items')
 
-#-----open(OperationParameters.txt,'a')
